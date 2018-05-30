3658be238d83c65adfe92f0d7b14ee11b9ddb0d5
Tom Westerhout <kot.tom97@gmail.com>
2018-05-31 00:28:58 +0200

    take_exactly now uses concepts; no support for integral_constant yet.

--- include/boost/static_views/take.hpp
+++ include/boost/static_views/take.hpp
@@ -1,4 +1,4 @@
-//          Copyright Tom Westerhout 2017.
+//          Copyright Tom Westerhout 2017-2018.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
@@ -10,58 +10,62 @@
 #ifndef BOOST_STATIC_VIEWS_TAKE_HPP
 #define BOOST_STATIC_VIEWS_TAKE_HPP
 
-#include "algorithm_base.hpp"
-#include "detail/config.hpp"
-#include "detail/utils.hpp"
-#include "sequence_traits.hpp"
 #include "view_base.hpp"
+
 #include <algorithm>
 #include <type_traits>
 
 BOOST_STATIC_VIEWS_BEGIN_NAMESPACE
 
 namespace detail {
-template <class View, std::ptrdiff_t Extent>
-struct take_impl : view_adaptor_base<take_impl<View, Extent>, View> {
+
+template <class Wrapper, std::ptrdiff_t Extent>
+struct take_view_impl
+    : view_adaptor_base<take_view_impl<Wrapper, Extent>, Wrapper> {
+
+  private:
+    using wrapper_type = Wrapper;
+    using base = view_adaptor_base<take_view_impl<Wrapper, Extent>, Wrapper>;
+
+  public:
+    using typename base::index_type;
+    using typename base::reference;
+    using typename base::size_type;
+    using typename base::value_type;
+
+    using base::parent;
 
     /// \brief Constructs a view of \p xs consisting of at most \p n
     /// elements of \p xs.
 
     /// \param xs **Rvalue** reference to a wrapper around a view.
-    /// \param n  Number of elements to take. The resulting view will
-    ///           contain `min(xs.get().size(), n)` elements.
-    ///
-    /// \exception Never throws, unless the initialisation of
-    /// #view_adaptor_base with `Wrapper&&` throws.
-    explicit BOOST_STATIC_VIEWS_CONSTEXPR take_impl(
-        View&& xs, std::size_t const n)
-#if defined(BOOST_STATIC_VIEWS_DISABLE_CHECKS)
-        BOOST_STATIC_VIEWS_NOEXCEPT_IF(std::is_nothrow_constructible<
-            typename take_impl::view_adaptor_base_type, View&&>::value
-            // This is formally wrong, but come on,
-            // std::min(size_t, size_t) _should_ be noexcept.
-            )
-#endif
-        : take_impl::view_adaptor_base_type{std::move(xs)}, _n{n}
+    /// \param n  Number of elements to take. `n` must not exceed the size of
+    ///           `xs`.
+    BOOST_STATIC_VIEWS_CONSTEXPR take_view_impl(
+        wrapper_type&& xs, index_type const n)
+        BOOST_STATIC_VIEWS_NOEXCEPT_CHECKS_IF(
+            std::is_nothrow_constructible<base, View&&>::value)
+        : base{std::move(xs)}, _n{n}
     {
-        BOOST_STATIC_VIEWS_EXPECT(n <= this->parent().size(),
-            BOOST_STATIC_VIEWS_BUG_MESSAGE);
         BOOST_STATIC_VIEWS_EXPECT(
-            extent() == dynamic_extent || extent() >= size(),
+            0 <= n && n <= static_cast<index_type>(parent().size()),
             BOOST_STATIC_VIEWS_BUG_MESSAGE);
     }
+
+    take_view_impl(take_view_impl const&) = default;
+    take_view_impl(take_view_impl&&)      = default;
+    take_view_impl& operator=(take_view_impl const&) = default;
+    take_view_impl& operator=(take_view_impl&&) = default;
 
     /// \brief Returns the number of elements viewed.
-    BOOST_STATIC_VIEWS_CONSTEXPR auto size() const noexcept
-        -> std::size_t
+    BOOST_STATIC_VIEWS_CONSTEXPR auto size() const noexcept -> size_type
     {
-        return _n;
+        return static_cast<size_type>(_n);
     }
 
-    BOOST_STATIC_VIEWS_CONSTEXPR static auto extent() noexcept
-        -> std::ptrdiff_t
+    static constexpr auto extent() noexcept -> std::ptrdiff_t
     {
-        return Extent;
+        return dynamic_extent;
     }
 
     /// \brief "Maps" index \p i to the corresponding index in the
@@ -69,68 +73,51 @@ struct take_impl : view_adaptor_base<take_impl<View, Extent>, View> {
 
     /// For \f$ i \geq \text{xs.size}()\f$ behavior of this function
     /// is undefined
-    BOOST_STATIC_VIEWS_FORCEINLINE
-    BOOST_STATIC_VIEWS_CONSTEXPR auto map(std::size_t const i) const
-        noexcept
+    BOOST_STATIC_VIEWS_CONSTEXPR auto map(index_type const i) const
+        BOOST_STATIC_VIEWS_NOEXCEPT_CHECKS_IF(true)
     {
-        BOOST_STATIC_VIEWS_EXPECT(i < size(),
-            "Which part of `map: [0, size()) --> N` you didn't "
-            "understand?");
+        BOOST_STATIC_VIEWS_EXPECT(0 <= i && i < static_cast<index_type>(size()),
+            "boost::static_views::take_view_impl::map: Precondition "
+            "`0 <= i < size()` is not satisfied.");
         return i;
     }
 
   private:
-    std::size_t _n;
+    index_type _n;
 };
 
-struct make_take_exactly_impl {
-    // clang-format off
-    template <class View, std::size_t N>
-    BOOST_STATIC_VIEWS_CONSTEXPR
-    auto operator()(View&& xs,
-        std::integral_constant<std::size_t, N> /*unused*/) const
-    // clang-format on
-#if defined(BOOST_STATIC_VIEWS_DISABLE_CHECKS)
-        BOOST_STATIC_VIEWS_NOEXCEPT_IF(noexcept(
-            take_impl<std::decay_t<View>, View::type::extent()>{
-                std::forward<View>(xs), N}))
-#endif
-    {
-        BOOST_STATIC_VIEWS_EXPECT(N <= xs.get().size(),
-            "take_exactly(n)(xs) expects `n <= xs.size()`.");
-        BOOST_STATIC_VIEWS_EXPECT(
-            View::type::extent() == dynamic_extent
-                || View::type::extent() >= xs.get().size(),
-            BOOST_STATIC_VIEWS_BUG_MESSAGE);
-        return take_impl<std::decay_t<View>,
-            ((View::type::extent() > static_cast<std::ptrdiff_t>(N))
-                    ? static_cast<std::ptrdiff_t>(N)
-                    : View::type::extent())>{
-            std::forward<View>(xs), N};
-    }
+struct take_exactly_impl {
+  private:
+    template <class Wrapper, class IndexType>
+    BOOST_STATIC_VIEWS_CONSTEXPR auto call_impl(Wrapper xs, IndexType n) const
+        BOOST_STATIC_VIEWS_AUTO_NOEXCEPT_RETURN(
+            take_view_impl<Wrapper, dynamic_extent>{std::move(xs), n});
 
+  public:
     // clang-format off
-    template <class View>
+    template <class V
+        BOOST_STATIC_VIEWS_REQUIRES(
+            View<std::remove_cv_t<std::remove_reference_t<V>>>)
     BOOST_STATIC_VIEWS_CONSTEXPR
-    auto operator()(View&& xs, std::size_t const n) const
-#if defined(BOOST_STATIC_VIEWS_DISABLE_CHECKS)
-        BOOST_STATIC_VIEWS_NOEXCEPT_IF(noexcept(
-            take_impl<std::decay_t<View>, View::type::extent()>{
+    auto operator()(V&& xs,
+        typename std::remove_cv_t<
+                 std::remove_reference_t<V>>::index_type const n) const
+        // clang-format on
+        BOOST_STATIC_VIEWS_NOEXCEPT_CHECKS_IF(
+            noexcept(take_impl<std::decay_t<View>, View::type::extent()>{
                 std::forward<View>(xs), n}))
-#endif
     {
-        BOOST_STATIC_VIEWS_EXPECT(n <= xs.get().size(),
-            "take_exactly(n)(xs) expects `n <= xs.size()`.");
+        using index_type =
+            typename std::remove_cv_t<std::remove_reference_t<V>>::index_type;
         BOOST_STATIC_VIEWS_EXPECT(
-            View::type::extent() == dynamic_extent
-                || View::type::extent() >= xs.get().size(),
-            BOOST_STATIC_VIEWS_BUG_MESSAGE);
-        return take_impl<std::decay_t<View>,
-            View::type::extent()>{std::forward<View>(xs), n};
+            0 <= n && n <= static_cast<index_type>(xs.size()),
+            "boost::static_views::take_exactly(xs, n): Precondition "
+            "`0 <= n <= xs.size()` is not satisfied.");
+        return call_impl(make_wrapper(std::forward<V>(xs)), n);
     }
-    // clang-format on
 };
 
+#if 0
 struct make_take_exactly_algo_impl {
     // clang-format off
     template <std::size_t N>
@@ -210,7 +197,7 @@ struct make_take_algo_impl {
     );
     // clang-format on
 };
-
+#endif
 } // end namespace detail
 
 /// \verbatim embed:rst:leading-slashes
@@ -221,10 +208,9 @@ struct make_take_algo_impl {
 /// first :math:`n` elements of :math:`xs`. If the size of :math:`xs`
 /// is less than :math:`n`, view of the whole :math:`xs` is returned.
 /// \endverbatim
-BOOST_STATIC_VIEWS_INLINE_VARIABLE(detail::make_take_algo_impl, take)
+// BOOST_STATIC_VIEWS_INLINE_VARIABLE(detail::make_take_algo_impl, take)
 
-BOOST_STATIC_VIEWS_INLINE_VARIABLE(
-    detail::make_take_exactly_algo_impl, take_exactly)
+BOOST_STATIC_VIEWS_INLINE_VARIABLE(detail::take_exactly_impl, take_exactly)
 
 BOOST_STATIC_VIEWS_END_NAMESPACE
 
