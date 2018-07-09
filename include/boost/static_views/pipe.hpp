f240c2be01a89affd89b05e48544add7f3663248
Tom Westerhout <kot.tom97@gmail.com>
2018-07-09 17:51:23 +0100

    Fixed ADL issues with piping syntax.

--- include/boost/static_views/pipe.hpp
+++ include/boost/static_views/pipe.hpp
@@ -12,9 +12,15 @@
 
 BOOST_STATIC_VIEWS_BEGIN_NAMESPACE
 
+// namespace detail here is needed to make ADL work. Functions like raw_view
+// return structs from boost::static_views::detail namespace rather than
+// boost::static_views.
+namespace detail {
+
 // clang-format off
 template <class V, class F
-    BOOST_STATIC_VIEWS_REQUIRES(View<V>)
+    BOOST_STATIC_VIEWS_REQUIRES(
+        View<std::remove_cv_t<std::remove_reference_t<V>>>)
 BOOST_STATIC_VIEWS_FORCEINLINE
 BOOST_STATIC_VIEWS_CONSTEXPR
 auto operator|(V&& xs, F&& pipe)
@@ -23,6 +29,10 @@ auto operator|(V&& xs, F&& pipe)
     invoke(std::forward<F>(pipe), std::forward<V>(xs))
 );
 // clang-format on
+
+} // namespace detail
+
+using detail::operator|;
 
 BOOST_STATIC_VIEWS_END_NAMESPACE
 
