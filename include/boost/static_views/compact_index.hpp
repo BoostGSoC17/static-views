6e030398dbd268e151dec411043bcd155a400486
Tom Westerhout <kot.tom97@gmail.com>
2018-08-13 11:53:35 +0100

    Add tagging to compact_index. This way we can safely derive from multiple compact_indices and benefit from EBO. NOTE: Compilers (yes, even Clang) have difficulty with this...

--- include/boost/static_views/compact_index.hpp
+++ include/boost/static_views/compact_index.hpp
@@ -10,10 +10,12 @@
 
 #include <algorithm>
 #include <type_traits>
+#include <utility>
 
 BOOST_STATIC_VIEWS_BEGIN_NAMESPACE
 
-template <class IndexType, std::ptrdiff_t Extent = dynamic_extent, class = void>
+template <class Tag, class IndexType, std::ptrdiff_t Extent = dynamic_extent,
+    class = void>
 struct compact_index {
   private:
     IndexType _payload;
@@ -50,8 +52,8 @@ struct compact_index {
     operator IndexType() const noexcept { return index(); }
 };
 
-template <class IndexType, std::ptrdiff_t Extent>
-struct compact_index<IndexType, Extent, std::enable_if_t<Extent >= 0>> {
+template <class Tag, class IndexType, std::ptrdiff_t Extent>
+struct compact_index<Tag, IndexType, Extent, std::enable_if_t<Extent >= 0>> {
   public:
     // clang-format off
     constexpr compact_index() noexcept = default;
@@ -71,34 +73,38 @@ struct compact_index<IndexType, Extent, std::enable_if_t<Extent >= 0>> {
     BOOST_STATIC_VIEWS_CONSTEXPR
     auto index() const noexcept { return static_cast<IndexType>(Extent); }
 
-    /*
+    static constexpr auto as_integral_constant() noexcept
+    {
+        return std::integral_constant<IndexType, Extent>{};
+    }
+
     BOOST_STATIC_VIEWS_PURE
     BOOST_STATIC_VIEWS_CONSTEXPR
     operator IndexType() const noexcept { return index(); }
-    */
 };
 
 // clang-format off
-template <class IndexType
+template <class Tag = void, class IndexType = int
     BOOST_STATIC_VIEWS_REQUIRES(std::is_integral<IndexType>::value)
 BOOST_STATIC_VIEWS_CONSTEXPR auto index(IndexType const x) noexcept
 // clang-format on
 {
-    return compact_index<IndexType>{x};
+    return compact_index<Tag, IndexType>{x};
 }
 
-template <class IndexType, IndexType I>
+template <class Tag = void, class IndexType = int, IndexType I = 0>
 BOOST_STATIC_VIEWS_CONSTEXPR auto index(
     std::integral_constant<IndexType, I> /*unused*/) noexcept
 {
-    return compact_index<IndexType, I>{};
+    return compact_index<Tag, IndexType, I>{};
 }
 
 #define BOOST_STATIC_VIEWS_DECLARE_BIN_OP(op)                                  \
-    template <class IndexType, std::ptrdiff_t ExtentA, std::ptrdiff_t ExtentB> \
+    template <class Tag, class IndexType, std::ptrdiff_t ExtentA,              \
+        std::ptrdiff_t ExtentB>                                                \
     BOOST_STATIC_VIEWS_PURE BOOST_STATIC_VIEWS_CONSTEXPR auto operator op(     \
-        compact_index<IndexType, ExtentA> const a,                             \
-        compact_index<IndexType, ExtentB> const b) noexcept->bool              \
+        compact_index<Tag, IndexType, ExtentA> const a,                        \
+        compact_index<Tag, IndexType, ExtentB> const b) noexcept->bool         \
     {                                                                          \
         return a.index() op b.index();                                         \
     }
@@ -113,18 +119,18 @@ BOOST_STATIC_VIEWS_DECLARE_BIN_OP(>=)
 #undef BOOST_STATIC_VIEWS_DECLARE_BIN_OP
 
 #define BOOST_STATIC_VIEWS_DECLARE_BIN_OP(op)                                  \
-    template <class IndexType, std::ptrdiff_t Extent>                          \
+    template <class Tag, class IndexType, std::ptrdiff_t Extent>               \
     BOOST_STATIC_VIEWS_PURE BOOST_STATIC_VIEWS_CONSTEXPR auto operator op(     \
-        compact_index<IndexType, Extent> const a,                              \
-        IndexType const                        b) noexcept->bool                                      \
+        compact_index<Tag, IndexType, Extent> const a,                         \
+        IndexType const                             b) noexcept->bool                                      \
     {                                                                          \
         return a.index() op b;                                                 \
     }                                                                          \
                                                                                \
-    template <class IndexType, std::ptrdiff_t Extent>                          \
+    template <class Tag, class IndexType, std::ptrdiff_t Extent>               \
     BOOST_STATIC_VIEWS_PURE BOOST_STATIC_VIEWS_CONSTEXPR auto operator op(     \
-        IndexType const                        a,                              \
-        compact_index<IndexType, Extent> const b) noexcept->bool               \
+        IndexType const                             a,                         \
+        compact_index<Tag, IndexType, Extent> const b) noexcept->bool          \
     {                                                                          \
         return a op b.index();                                                 \
     }
